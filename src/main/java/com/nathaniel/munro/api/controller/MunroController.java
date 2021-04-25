0faf7ae288d3bed758c388337f0f7d89299678d9
NathanielGSymons <n.gsymons@gmail.com>
2021-04-25 15:43:31 +0100

    replaced @Controller with @RestController

--- src/main/java/com/nathaniel/munro/api/controller/MunroController.java
+++ src/main/java/com/nathaniel/munro/api/controller/MunroController.java
@@ -4,16 +4,12 @@ import com.nathaniel.munro.api.model.Munro;
 import com.nathaniel.munro.api.service.MunroService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class MunroController {
     private final MunroService munroService;
@@ -23,7 +19,6 @@ public class MunroController {
     }
 
     @GetMapping(value = "/munros", produces = "application/json")
-    @ResponseBody
     public ResponseEntity<List<Munro>> getMunros(
             @RequestParam(required = false) Map<String, String> parameters
     ) {
